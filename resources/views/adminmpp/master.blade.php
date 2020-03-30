29bc76b4c41742c78915c0aeec5e64d516d70bc0
herry88 <herry081288@gmail.com>
2020-03-30 10:06:55 +0700

    NewAdd

--- resources/views/adminmpp/master.blade.php
+++ resources/views/adminmpp/master.blade.php
@@ -10,7 +10,6 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
         <link rel="shortcut icon" href="assets/images/favicon.ico">
-
         <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
         <link href="assets/css/icons.css" rel="stylesheet" type="text/css">
         <link href="assets/css/style.css" rel="stylesheet" type="text/css">
